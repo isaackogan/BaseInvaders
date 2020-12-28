b4fef7a5c2a88786354d9e353ee83fa1123f6f92
isaac <isaacikogan@gmail.com>
2020-12-28 05:25:54 -0500

    Changed Font

--- config.py
+++ config.py
@@ -9,6 +9,6 @@ COLOUR_TAN = (225, 180, 128)  # Middle colour
 COLOUR_BROWN = (88, 47, 12)  # Text Colour
 
 COLOUR_BLACK = (0, 0, 0)
-COLOUR_RED_BLACK = (50, 20, 20)
+COLOUR_RED_BLACK = ()
 
-pause_menu_font = pygame.font.Font("./BaseInvaders/resources/fonts/Roboto-Regular.ttf", 50)
+pause_menu_font = pygame.font.Font("./BaseInvaders/resources/fonts/bahnschrift.ttf", 50)
